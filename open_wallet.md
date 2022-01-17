3da009fac51b96ec677c620a18351b5080f782f2
Anthony Brochu <anthony.brochu@outlook.com>
2022-01-16 20:38:48 -0500

    first commit

---
+++ open_wallet.md
@@ -0,0 +1,21 @@
+# Create Wallet
+
+cleos wallet create --to-console
+
+# Open the wallet
+
+cleos wallet open
+
+cleos wallet list
+
+# Unlock a wallet
+
+cleos wallet unlock
+
+# Import keys into a Wallet
+
+cleos wallet create_key
+
+cleos wallet import
+
+
---
+++ start_blockchain.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# This command is for developement purposes only.
+
+# You can find troubleshooting here : 
+# https://developers.eos.io/welcome/latest/getting-started-guide/local-development-environment/start-nodeos-keosd/#starting-nodeos
+
+if [ "$1" = 'replay' ]; then
+
+nodeos -e -p eosio \
+--plugin eosio::producer_plugin \
+--plugin eosio::producer_api_plugin \
+--plugin eosio::chain_api_plugin \
+--plugin eosio::http_plugin \
+--plugin eosio::history_plugin \
+--plugin eosio::history_api_plugin \
+--filter-on="*" \
+--access-control-allow-origin='*' \
+--contracts-console \
+--http-validate-host=false \
+--replay-blockchain \
+--verbose-http-errors >> nodeos.log 2>&1 &
+
+elif [ "$1" = 'delete' ]; then
+
+nodeos -e -p eosio \
+--plugin eosio::producer_plugin \
+--plugin eosio::producer_api_plugin \
+--plugin eosio::chain_api_plugin \
+--plugin eosio::http_plugin \
+--plugin eosio::history_plugin \
+--plugin eosio::history_api_plugin \
+--filter-on="*" \
+--access-control-allow-origin='*' \
+--contracts-console \
+--http-validate-host=false \
+--delete-all-blocks \
+--verbose-http-errors >> nodeos.log 2>&1 &
+
+else
+
+nodeos -e -p eosio \
+--plugin eosio::producer_plugin \
+--plugin eosio::producer_api_plugin \
+--plugin eosio::chain_api_plugin \
+--plugin eosio::http_plugin \
+--plugin eosio::history_plugin \
+--plugin eosio::history_api_plugin \
+--filter-on="*" \
+--access-control-allow-origin='*' \
+--contracts-console \
+--http-validate-host=false \
+--verbose-http-errors >> nodeos.log 2>&1 &
+
+fi
